faba2de2563a4e7b5b8542e6efc9c91ca46541fa
Rishabh <rbnext29@gmail.com>
2018-12-01 23:36:10 -0500

    adding limits dataset

---
+++ datasets/limits.py
@@ -0,0 +1,11 @@
+from torch.utils.data import Dataset
+class LimitDataset(Dataset):
+    def __init__(self, dataset, n):
+        self.dataset = dataset
+        self.n = n
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, i):
+        return self.dataset[i]
